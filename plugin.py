9868b63f125a38bf708d40b300452e84138ff2ed
Erwanweb <44497874+Erwanweb@users.noreply.github.com>
2025-06-25 10:28:09 +0200

    Update plugin.py

--- plugin.py
+++ plugin.py
@@ -110,7 +110,7 @@ class BasePlugin:
                 return
 
             # ✅ Toujours prendre la première mesure (la plus récente)
-            data = data_list[47]
+            data = data_list[0]
 
             # Vérification de l’ancienneté de la donnée du flipr
             try:
